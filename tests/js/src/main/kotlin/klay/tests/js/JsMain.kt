9be8a5e0d5f835f93fc94fdd94be450f847ed725
Christoph Dietze <christoph.dietze@kurzdigital.com>
2017-04-26 21:27:23 +0200

    A yellow background using webgl

---
+++ tests/js/src/main/kotlin/klay/tests/js/JsMain.kt
@@ -0,0 +1,15 @@
+package klay.tests.js
+
+//fun main(args: Array<String>) {
+//    return HelloWebglMain.main(args)
+//    println("Hello Kotlin on JavaScript!")
+//
+//    val plat = JsPlatform()
+//    val game = Game(plat)
+//    plat.run(game)
+//
+//    game.sayHello()
+//}
+
+fun main(args: Array<String>) = HelloWebglMain.main(args)
+
