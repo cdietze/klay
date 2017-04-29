f19c3b3e23f44362ae50dc066af08e29bb006c66
Christoph Dietze <git@cdietze.de>
2017-04-29 23:02:40 +0200

    Make pythagoras.util kotlin JS compatible

--- pythagoras/src/main/kotlin/pythagoras/util/Platform.kt
+++ pythagoras/src/main/kotlin/pythagoras/util/Platform.kt
@@ -12,42 +12,41 @@ object Platform {
      * Returns a hash code for the supplied float value.
      */
     fun hashCode(f1: Float): Int {
-        return java.lang.Float.floatToIntBits(f1)
+        return f1.hashCode()
     }
 
     /**
      * Returns a hash code for the supplied double value.
      */
     fun hashCode(d1: Double): Int {
-        val bits = java.lang.Double.doubleToLongBits(d1)
-        return (bits xor bits.ushr(32)).toInt()
+        return d1.hashCode()
     }
 
     /**
      * Clones the supplied array of bytes.
      */
     fun clone(values: ByteArray): ByteArray {
-        return values.clone()
+        return values.copyOf()
     }
 
     /**
      * Clones the supplied array of ints.
      */
     fun clone(values: IntArray): IntArray {
-        return values.clone()
+        return values.copyOf()
     }
 
     /**
      * Clones the supplied array of floats.
      */
     fun clone(values: FloatArray): FloatArray {
-        return values.clone()
+        return values.copyOf()
     }
 
     /**
      * Clones the supplied array of doubles.
      */
     fun clone(values: DoubleArray): DoubleArray {
-        return values.clone()
+        return values.copyOf()
     }
 }
