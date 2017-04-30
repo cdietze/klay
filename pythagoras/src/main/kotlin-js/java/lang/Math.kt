38f32b4b7a75a0b7a2d82717822c5de58ca80c37
Christoph Dietze <christoph.dietze@kurzdigital.com>
2017-04-30 22:59:24 +0200

    Add more licensing notices

--- pythagoras/src/main/kotlin-js/java/lang/Math.kt
+++ pythagoras/src/main/kotlin-js/java/lang/Math.kt
@@ -7,6 +7,21 @@ import kotlin.js.Math as JsMath
  *
  * Implementation needed for JS backend.
  * Based on [Code taken from GWT project](https://github.com/gwtproject/gwt/blob/master/user/super/com/google/gwt/emul/java/lang/Math.java)
+ * for which the following applies:
+ *
+ * Copyright 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 object Math {
     // The following methods are not implemented because JS doesn't provide the
