c8e65766a4f2e472a7422cf485f51bc30649fccf
Christoph Dietze <git@cdietze.de>
2017-04-30 22:21:26 +0200

    Comments

--- pythagoras/src/main/kotlin-js/java/lang/System.kt
+++ pythagoras/src/main/kotlin-js/java/lang/System.kt
@@ -1,5 +1,8 @@
 package java.lang
 
+/**
+ * Implementation needed for JS backend.
+ */
 object System {
     /** Not quite the original signature which uses `java.lang.Object`, but we really just want to copy arrays */
     fun arraycopy(src: FloatArray, srcPos: Int, dest: FloatArray, destPos: Int, length: Int): Unit {
