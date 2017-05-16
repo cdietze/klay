f1c2679b97add16a693dc6654450bc7e3d2d8a64
Christoph Dietze <christoph.dietze@gmail.com>
2017-05-16 21:56:59 +0200

    Comment out JS stuff so it compiles again

--- tests/js/src/main/kotlin/klay/tests/js/JsMain.kt
+++ tests/js/src/main/kotlin/klay/tests/js/JsMain.kt
@@ -1,13 +1,12 @@
 package klay.tests.js
 
-import klay.core.Game
 import klay.js.JsPlatform
 
 fun main(args: Array<String>) {
     println("Hello Kotlin on JavaScript!")
     val plat = JsPlatform()
-    Game(plat)
-    plat.run()
+//    Game(plat)
+//    plat.run()
 }
 
 //fun main(args: Array<String>) = HelloWebglMain.main(args)
