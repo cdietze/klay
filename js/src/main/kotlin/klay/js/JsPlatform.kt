e3479e429dcbb8e91b45b9db7319b10d5c453054
Christoph Dietze <git@cdietze.de>
2017-05-16 21:56:59 +0200

    Comment out JS stuff so it compiles again

--- js/src/main/kotlin/klay/js/JsPlatform.kt
+++ js/src/main/kotlin/klay/js/JsPlatform.kt
@@ -1,23 +1,22 @@
 package klay.js
 
-import klay.core.GL20
-import klay.core.Graphics
-import klay.core.Platform
 import org.khronos.webgl.WebGLRenderingContext
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.HTMLDivElement
+import pythagoras.f.Point
 import kotlin.browser.document
 import kotlin.browser.window
 
-class JsPlatform : Platform() {
+class JsPlatform {
 
-    private val webGL: WebGLRenderingContext
+    private var webGL: WebGLRenderingContext? = null
 
     val width = 300
     val height = 300
 
     init {
         println("Initializing JsPlatform")
+        println("p1: ${Point(1f, 1.234567f)}, p2: ${Point(2f, 1.2000000f)}")
 
         val root: HTMLDivElement = document.getElementById("klay-root") as HTMLDivElement? ?: document.body!!.appendChild(document.createElement("div")) as HTMLDivElement
 
@@ -27,27 +26,15 @@ class JsPlatform : Platform() {
         canvas.height = height
 
         webGL = canvas.getContext("webgl") as WebGLRenderingContext
-        webGL.clearColor(0f, 0f, 0f, 1f)
+        webGL!!.clearColor(0f, 0f, 0f, 1f)
 
-    }
-
-    override val graphics: Graphics = object : Graphics {
-
-        override val gl: GL20 get() = TODO("not implemented")
-//        override val gl = TODO()
-//                object : GL20() {
-//            override fun glClearColor(red: Float, green: Float, blue: Float, alpha: Float) {
-//                webGL.clearColor(red, green, blue, alpha)
-//            }
-//        }
     }
 
     fun run() {
         fun animate() {
-            frameSignal.emit(this)
-            webGL.viewport(0, 0, width, height)
-            webGL.clear(WebGLRenderingContext.COLOR_BUFFER_BIT)
-            webGL.flush()
+            webGL!!.viewport(0, 0, width, height)
+            webGL!!.clear(WebGLRenderingContext.COLOR_BUFFER_BIT)
+            webGL!!.flush()
             window.requestAnimationFrame { animate() }
         }
         animate()
