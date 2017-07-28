5df1ae9d72e3d0191f65e832f1d13df40c87d6bf
Christoph Dietze <christoph.dietze@gmail.com>
2017-07-28 23:11:03 +0200

    Add overloaded methods that utilize pythagoras' types

--- core/src/main/kotlin/klay/core/Path.kt
+++ core/src/main/kotlin/klay/core/Path.kt
@@ -1,5 +1,7 @@
 package klay.core
 
+import pythagoras.f.XY
+
 /**
  * A path object created by [Canvas.createPath].
  */
@@ -24,10 +26,22 @@ interface Path {
     fun moveTo(x: Float, y: Float): Path
 
     /**
+     * Moves the position to the given location.
+     * @return this path for convenient call chaining.
+     */
+    fun moveTo(location: XY): Path = moveTo(location.x, location.y)
+
+    /**
      * Adds a line to the path, from the current position to the specified target.
      * @return this path for convenient call chaining.
      */
     fun lineTo(x: Float, y: Float): Path
+
+    /**
+     * Adds a line to the path, from the current position to the specified target.
+     * @return this path for convenient call chaining.
+     */
+    fun lineTo(location: XY): Path = lineTo(location.x, location.y)
 
     /**
      * Adds a quadratic curve to the path, from the current position to the specified target, with
@@ -37,11 +51,25 @@ interface Path {
     fun quadraticCurveTo(cpx: Float, cpy: Float, x: Float, y: Float): Path
 
     /**
+     * Adds a quadratic curve to the path, from the current position to the specified target, with
+     * the specified control point.
+     * @return this path for convenient call chaining.
+     */
+    fun quadraticCurveTo(cp: XY, target: XY): Path = quadraticCurveTo(cp.x, cp.y, target.x, target.y)
+
+    /**
      * Adds a bezier curve to the path, from the current position to the specified target, using the
      * supplied control points.
      * @return this path for convenient call chaining.
      */
     fun bezierTo(c1x: Float, c1y: Float, c2x: Float, c2y: Float, x: Float, y: Float): Path
+
+    /**
+     * Adds a bezier curve to the path, from the current position to the specified target, using the
+     * supplied control points.
+     * @return this path for convenient call chaining.
+     */
+    fun bezierTo(c1: XY, c2: XY, target: XY): Path = bezierTo(c1.x, c1.y, c2.x, c2.y, target.x, target.y)
 
     // TODO(jgw): fill rules (HTML Canvas doesn't seem to have anything)
     // Android has [inverse] winding, even-odd
