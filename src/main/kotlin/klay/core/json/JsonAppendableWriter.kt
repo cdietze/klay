6ac9578bc8e3ee51a240dfb60acbef168fef031d
Christoph Dietze <christoph.dietze@gmail.com>
2017-11-04 22:51:37 +0100

    Remove references to Flushable in `JsonAppendableWriter`

--- src/main/kotlin/klay/core/json/JsonAppendableWriter.kt
+++ src/main/kotlin/klay/core/json/JsonAppendableWriter.kt
@@ -1,13 +1,9 @@
 package klay.core.json
 
-// TODO(cdi): Remove this class: with [Flushable] not being in kotlin common, this class doesnt make much sense
 internal class JsonAppendableWriter(appendable: Appendable) : JsonWriterBase<JsonAppendableWriter>(appendable) {
 
     /**
-     * Closes this JSON writer and flushes the underlying [Appendable] if it is also [Flushable].
-
-     * @throws JsonWriterException
-     * *             if the underlying [Flushable] [Appendable] failed to flush.
+     * Closes this JSON writing session.
      */
     fun done() {
         super.doneInternal()
